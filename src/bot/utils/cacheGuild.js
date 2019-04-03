46f0ec5ec86d577f95247fe38cf18c7049135888
Piero <caf203@gmail.com>
2019-04-03 12:21:48 -0400

    wew

--- src/bot/utils/cacheGuild.js
+++ src/bot/utils/cacheGuild.js
@@ -1,6 +1,5 @@
-const getGuildDocument = require('../../db/interfaces/postgres/read').getGuild
-
 module.exports = async guildID => {
+  const getGuildDocument = require('../../db/interfaces/postgres/read').getGuild
   const GuildSettings = require('../bases/GuildSettings') // this for some reason
   const doc = await getGuildDocument(guildID)
   global.bot.guildSettingsCache[guildID] = new GuildSettings(doc)
