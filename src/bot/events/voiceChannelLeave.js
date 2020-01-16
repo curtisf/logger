6c04bef8e05c314464a36ba2232819746db630f1
Curtis Fowler <caf203@gmail.com>
2020-01-16 14:06:44 -0500

    emergency patch for member leave being wonky

--- src/bot/events/voiceChannelLeave.js
+++ src/bot/events/voiceChannelLeave.js
@@ -4,24 +4,26 @@ module.exports = {
   name: 'voiceChannelLeave',
   type: 'on',
   handle: async (member, channel) => {
-    await send({
-      guildID: channel.guild.id,
-      eventName: 'voiceChannelLeave',
-      embed: {
-        author: {
-          name: `${member.username}#${member.discriminator} ${member.nick ? `(${member.nick})` : ''}`,
-          icon_url: member.avatarURL
-        },
-        description: `**${member.username}#${member.discriminator}** ${member.nick ? `(${member.nick})` : ''} left voice channel: ${channel.name}.`,
-        fields: [{
-          name: 'Channel',
-          value: `<#${channel.id}> (${channel.name})`
-        }, {
-          name: 'ID',
-          value: `\`\`\`ini\nUser = ${member.id}\nChannel = ${channel.id}\`\`\``
-        }],
-        color: 3553599
-      }
-    })
+    if (member) {
+      await send({
+        guildID: channel.guild.id,
+        eventName: 'voiceChannelLeave',
+        embed: {
+          author: {
+            name: `${member.username}#${member.discriminator} ${member.nick ? `(${member.nick})` : ''}`,
+            icon_url: member.avatarURL
+          },
+          description: `**${member.username}#${member.discriminator}** ${member.nick ? `(${member.nick})` : ''} left voice channel: ${channel.name}.`,
+          fields: [{
+            name: 'Channel',
+            value: `<#${channel.id}> (${channel.name})`
+          }, {
+            name: 'ID',
+            value: `\`\`\`ini\nUser = ${member.id}\nChannel = ${channel.id}\`\`\``
+          }],
+          color: 3553599
+        }
+      })
+    }
   }
 }
