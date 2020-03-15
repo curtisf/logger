39b0e90344bc81362ed1002cb39cf830fcad4737
Curtis Fowler <caf203@gmail.com>
2020-03-15 15:29:05 -0400

    add ratelimiter offset

--- src/bot/index.js
+++ src/bot/index.js
@@ -17,20 +17,20 @@ if (process.env.SENTRY_URI) {
   global.logger.warn('No Sentry URI provided. Error logging will be restricted to messages only.')
 }
 
-function connect() {
-  redisLock.lock('loggerinit', process.env.REDIS_LOCK_TTL).then(function(lock) {
+function connect () {
+  redisLock.lock('loggerinit', process.env.REDIS_LOCK_TTL).then(function (lock) {
     global.logger.startup(`Shards ${cluster.worker.rangeForShard} have obtained a lock and are connecting now. Configured Redis TTL is ${process.env.REDIS_LOCK_TTL}ms.`)
     global.bot.connect()
     global.bot.once('ready', () => {
-      lock.unlock().catch(function(err) {
-        global.logger.warn('Error while unlocking loggerinit on shards ' + cluster.worker.rangeForShard)
+      lock.unlock().catch(function (err) {
+        global.logger.warn(cluster.worker.rangeForShard + ' could not unlock, waiting')
+      })
     })
-    })
-}).catch(e => {
-  setTimeout(() => {
-    connect()
-  }, 15000)
-}) // throw out not being able to obtain a lock.
+  }).catch(e => {
+    setTimeout(() => {
+      connect()
+    }, 15000)
+  }) // throw out not being able to obtain a lock.
 }
 
 async function init () {
@@ -44,7 +44,8 @@ async function init () {
     restMode: true,
     messageLimit: 0,
     autoreconnect: true,
-    getAllUsers: true
+    getAllUsers: true,
+    ratelimiterOffset: 200
   })
 
   global.bot.editStatus('dnd', {
@@ -64,9 +65,8 @@ async function init () {
 
   require('../miscellaneous/bezerk')
 
-
   connect()
- 
+
   // const oldMessagesDeleted = await deleteMessagesOlderThanDays(process.env.MESSAGE_HISTORY_DAYS) debating on removing these
   // global.logger.info(`${oldMessagesDeleted} messages were deleted due to being older than ${process.env.MESSAGE_HISTORY_DAYS} day(s).`)
 }
@@ -83,12 +83,12 @@ process.on('SIGINT', async () => {
 
 process.on('unhandledRejection', (e) => {
   console.error(e)
-  if (!e.message.includes('[50013]') && !e.message.startsWith('Request timed out') && !e.message.startsWith('500 INTERNAL SERVER ERROR')) Raven.captureException(e.stack, {level: 'error'}) // handle when Discord freaks out
+  if (!e.message.includes('[50013]') && !e.message.startsWith('Request timed out') && !e.message.startsWith('500 INTERNAL SERVER ERROR')) Raven.captureException(e.stack, { level: 'error' }) // handle when Discord freaks out
 })
 
 process.on('uncaughtException', (e) => {
   console.error(e)
-  if (!e.message.includes('[50013]')) Raven.captureException(e.stack, {level: 'fatal'})
+  if (!e.message.includes('[50013]')) Raven.captureException(e.stack, { level: 'fatal' })
 })
 
 init()
