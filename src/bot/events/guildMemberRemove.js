56b6cccac4d4aea45ae0c9413155e3e079cca2fa
Piero <caf203@gmail.com>
2021-05-28 09:00:13 -0400

    feat: remove lurker check

--- src/bot/events/guildMemberRemove.js
+++ src/bot/events/guildMemberRemove.js
@@ -74,7 +74,7 @@ module.exports = {
           icon_url: member.avatarURL
         },
         color: 16711680,
-        description: `${member.username}#${member.discriminator} left ${guild.features.includes('DISCOVERABLE') ? '(possibly a lurker)' : ''}`,
+        description: `${member.username}#${member.discriminator} left`,
         fields: [{
           name: 'User Information',
           value: `${member.username}#${member.discriminator} (${member.id}) ${member.mention} ${member.bot ? '\nIs a bot' : ''}`
