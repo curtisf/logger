4cc7212dfdf6bf73fa5bd897a506edf2bcfc5272
Curtis Fowler <caf203@gmail.com>
2021-07-28 17:30:32 -0400

    fix: stifle duplicate messages (interactions)

--- src/db/messageBatcher.js
+++ src/db/messageBatcher.js
@@ -2,39 +2,39 @@ const format = require('pg-format')
 const pool = require('./clients/postgres')
 const aes = require('./aes')
 const BATCH_SIZE = process.env.MESSAGE_BATCH_SIZE || 1000
-let batch = []
+const batch = []
 
-async function addItem(messageAsArray) {
-    batch.push(messageAsArray)
-    if (batch.length >= BATCH_SIZE) {
-        await submitBatch()
-    }
+async function addItem (messageAsArray) {
+  batch.push(messageAsArray)
+  if (batch.length >= BATCH_SIZE) {
+    await submitBatch()
+  }
 }
 
-async function submitBatch() {
-    let toSubmit = batch.splice(0, process.env.MESSAGE_BATCH_SIZE)
-    await pool.query(format('INSERT INTO messages (id, author_id, content, attachment_b64, ts) VALUES %L', toSubmit))
+async function submitBatch () {
+  const toSubmit = batch.splice(0, process.env.MESSAGE_BATCH_SIZE)
+  await pool.query(format('INSERT INTO messages (id, author_id, content, attachment_b64, ts) VALUES %L ON CONFLICT DO NOTHING', toSubmit))
 }
 
-function getMessage(messageID) {
-    const message = batch.find(m => m[0] === messageID)
-    if (!message) return
-    return {
-        id: message[0],
-        author_id: message[1],
-        content: aes.decrypt(message[2]),
-        attachment_b64: '',
-        ts: Date.parse(message[4])
-    }
+function getMessage (messageID) {
+  const message = batch.find(m => m[0] === messageID)
+  if (!message) return
+  return {
+    id: message[0],
+    author_id: message[1],
+    content: aes.decrypt(message[2]),
+    attachment_b64: '',
+    ts: Date.parse(message[4])
+  }
 }
 
-function updateMessage(messageID, content) {
-    for (let i = 0; i < batch.length; i++) {
-        if (batch[i][0] === messageID) {
-            batch[i][2] = aes.encrypt(content ? content : 'None')
-            break
-        }
+function updateMessage (messageID, content) {
+  for (let i = 0; i < batch.length; i++) {
+    if (batch[i][0] === messageID) {
+      batch[i][2] = aes.encrypt(content || 'None')
+      break
     }
+  }
 }
 
 exports.getMessage = getMessage
