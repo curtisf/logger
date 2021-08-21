f49ed00b2d4849598d0b07d6f7de4cf9c48de781
Curtis Fowler <caf203@gmail.com>
2021-08-21 10:46:41 -0400

    fix: bulk queuer support multiple embeds

--- src/bot/modules/bulkqueue.js
+++ src/bot/modules/bulkqueue.js
@@ -9,26 +9,26 @@ const webhookIDToCharLimit = new Map()
 module.exports = async (senderPkg, guildSettings) => {
   if (webhookIDToQueue.has(senderPkg.webhookID)) {
     const updatedQueue = webhookIDToQueue.get(senderPkg.webhookID)
-    const charsToPush = getEmbedCharLens(senderPkg.embed)
+    const charsToPush = getEmbedCharLens(senderPkg.embeds)
     if (updatedQueue.length < 10 && webhookIDToCharLimit.get(senderPkg.webhookID) + charsToPush <= 5000) {
       updatedQueue.push(senderPkg)
       webhookIDToCharLimit.set(senderPkg.webhookID, webhookIDToCharLimit.get(senderPkg.webhookID) + charsToPush)
       webhookIDToQueue.set(senderPkg.webhookID, updatedQueue)
       clearTimeout(webhookIDToTimeout.get(senderPkg.webhookID))
       webhookIDToTimeout.set(senderPkg.webhookID, setTimeout(() => {
-        sendBulkLog(senderPkg, webhookIDToQueue.get(senderPkg.webhookID).map(p => p.embed), guildSettings)
+        sendBulkLog(senderPkg, webhookIDToQueue.get(senderPkg.webhookID).map(p => p.embeds).flat(1), guildSettings)
         webhookIDToQueue.delete(senderPkg.webhookID)
         webhookIDToTimeout.delete(senderPkg.webhookID)
         webhookIDToCharLimit.delete(senderPkg.webhookID)
       }, 5000))
     } else if (updatedQueue.length === 10 || webhookIDToCharLimit.get(senderPkg.webhookID) + charsToPush >= 5000) { // the limit is 6000 but let's be safe
       // queue is full, send log now.
-      sendBulkLog(senderPkg, updatedQueue.map(p => p.embed), guildSettings)
+      sendBulkLog(senderPkg, updatedQueue.map(p => p.embeds).flat(1), guildSettings)
       webhookIDToQueue.set(senderPkg.webhookID, [senderPkg])
       clearTimeout(webhookIDToTimeout.get(senderPkg.webhookID))
-      webhookIDToCharLimit.set(senderPkg.webhookID, getEmbedCharLens(senderPkg.embed))
+      webhookIDToCharLimit.set(senderPkg.webhookID, getEmbedCharLens(senderPkg.embeds))
       webhookIDToTimeout.set(senderPkg.webhookID, setTimeout(() => {
-        sendBulkLog(senderPkg, webhookIDToQueue.get(senderPkg.webhookID).map(p => p.embed), guildSettings)
+        sendBulkLog(senderPkg, webhookIDToQueue.get(senderPkg.webhookID).map(p => p.embeds).flat(1), guildSettings)
         webhookIDToQueue.delete(senderPkg.webhookID)
         webhookIDToTimeout.delete(senderPkg.webhookID)
         webhookIDToCharLimit.delete(senderPkg.webhookID)
@@ -36,10 +36,10 @@ module.exports = async (senderPkg, guildSettings) => {
     }
   } else {
     webhookIDToQueue.set(senderPkg.webhookID, [senderPkg])
-    webhookIDToCharLimit.set(senderPkg.webhookID, getEmbedCharLens(senderPkg.embed))
+    webhookIDToCharLimit.set(senderPkg.webhookID, getEmbedCharLens(senderPkg.embeds))
     clearTimeout(webhookIDToTimeout.get(senderPkg.webhookID))
     webhookIDToTimeout.set(senderPkg.webhookID, setTimeout(() => {
-      sendBulkLog(senderPkg, webhookIDToQueue.get(senderPkg.webhookID).map(p => p.embed), guildSettings)
+      sendBulkLog(senderPkg, webhookIDToQueue.get(senderPkg.webhookID).map(p => p.embeds).flat(1), guildSettings)
       webhookIDToQueue.delete(senderPkg.webhookID)
       webhookIDToTimeout.delete(senderPkg.webhookID)
       webhookIDToCharLimit.delete(senderPkg.webhookID)
@@ -47,22 +47,25 @@ module.exports = async (senderPkg, guildSettings) => {
   }
 }
 
-function getEmbedCharLens (embed) {
+function getEmbedCharLens (embeds) {
   let total = 0
-  for (const prop in embed) {
-    if (typeof embed[prop] === 'string') {
-      total += embed[prop].length
+  embeds.forEach(embed => {
+    for (const prop in embed) {
+      if (typeof embed[prop] === 'string') {
+        total += embed[prop].length
+      }
     }
-  }
-  for (let i = 0; i < (embed.fields ? embed.fields.length : 0); i++) {
-    if (typeof embed.fields[i].name === 'string') {
-      total += embed.fields[i].name.length
+    for (let i = 0; i < (embed.fields ? embed.fields.length : 0); i++) {
+      if (typeof embed.fields[i].name === 'string') {
+        total += embed.fields[i].name.length
+      }
+      if (typeof embed.fields[i].value === 'string') {
+        total += embed.fields[i].value.length
+      }
     }
-    if (typeof embed.fields[i].value === 'string') {
-      total += embed.fields[i].value.length
-    }
-  }
-  return total
+  })
+  return total > 6000 ? 1000 : total // if the char len of these embeds is greater than the max for all embeds in a single message
+  // return 1000 so it can silently fail (I have a TODO for this!)
 }
 
 function sendBulkLog (senderPkg, embeds, guildSettings) {
