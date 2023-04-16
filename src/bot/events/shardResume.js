0e325735bb5731326b022e48bbc68cabf404281b
Curtis Fowler <caf203@gmail.com>
2023-04-16 18:35:23 -0400

    lol

--- src/bot/events/shardResume.js
+++ src/bot/events/shardResume.js
@@ -6,6 +6,6 @@ module.exports = {
     handle: async (shardID) => {
       const cachedShard = global.bot.shards.get(shardID)
       global.logger.info(`[${cluster.worker.rangeForShard}] Shard ${shardID} has resumed`, err)
-      global.webhook.warn(`[${cluster.worker.rangeForShard}] Shard ${shardID} is has resumed\n\nResume -> ${cachedShard.resumeURL}\nStatus -> ${cachedShard.status}\nReconnecting? -> ${cachedShard.connecting ? 'yes' : 'no'}\nLast heartbeat sent: <t:${Math.ceil(cachedShard.lastHeartbeatSent / 1000)}>\nLast heartbeat received: <t:${Math.ceil(cachedShard.lastHeartbeatReceived / 1000)}>`)
+      global.webhook.warn(`[${cluster.worker.rangeForShard}] Shard ${shardID} has resumed\n\nResume -> ${cachedShard.resumeURL}\nStatus -> ${cachedShard.status}\nReconnecting? -> ${cachedShard.connecting ? 'yes' : 'no'}\nLast heartbeat sent: <t:${Math.ceil(cachedShard.lastHeartbeatSent / 1000)}>\nLast heartbeat received: <t:${Math.ceil(cachedShard.lastHeartbeatReceived / 1000)}>`)
     }
   }
