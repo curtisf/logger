42c567e212308f784245bae13aaad4b48bb7e67c
Curtis Fowler <caf203@gmail.com>
2023-04-16 18:40:42 -0400

    no undefined err

--- src/bot/events/shardResume.js
+++ src/bot/events/shardResume.js
@@ -5,7 +5,7 @@ module.exports = {
     type: 'on',
     handle: async (shardID) => {
       const cachedShard = global.bot.shards.get(shardID)
-      global.logger.info(`[${cluster.worker.rangeForShard}] Shard ${shardID} has resumed`, err)
+      global.logger.info(`[${cluster.worker.rangeForShard}] Shard ${shardID} has resumed`)
       global.webhook.warn(`[${cluster.worker.rangeForShard}] Shard ${shardID} has resumed\n\nResume -> ${cachedShard.resumeURL}\nStatus -> ${cachedShard.status}\nReconnecting? -> ${cachedShard.connecting ? 'yes' : 'no'}\nLast heartbeat sent: <t:${Math.ceil(cachedShard.lastHeartbeatSent / 1000)}>\nLast heartbeat received: <t:${Math.ceil(cachedShard.lastHeartbeatReceived / 1000)}>`)
     }
   }
