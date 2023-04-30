6b473c8db4d1edf13000cedc921231afe46054b4
Curtis Fowler <caf203@gmail.com>
2023-04-30 00:09:51 -0400

    shard health check

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -2,6 +2,8 @@ const cluster = require('cluster')
 const checkForMissingSettings = require('../utils/recoverSettings')
 const statAggregator = require('../modules/statAggregator')
 
+let failedHealthCheckCount = 0
+
 module.exports = {
   name: 'ready',
   type: 'once',
@@ -10,10 +12,23 @@ module.exports = {
     global.logger.info(`Worker instance hosting ${cluster.worker.rangeForShard} on id ${cluster.worker.id} is now ready to serve requests. This shard or shard range has ${global.bot.guilds.size} guilds and ${global.bot.users.size} users cached.`)
     global.webhook.generic(`Worker instance hosting ${cluster.worker.rangeForShard} on id ${cluster.worker.id} is now ready to serve requests. This shard or shard range has ${global.bot.guilds.size} guilds and ${global.bot.users.size} users cached.`)
     global.bot.editStatus('online', {
-      name: `Use /help | ${cluster.worker.rangeForShard} | Watching ${global.bot.guilds.size} guilds`
+      name: `Use /usage | ${cluster.worker.rangeForShard} | Watching ${global.bot.guilds.size} guilds`
     })
     if (global.bot.shards.find(s => s.id === 0)) { // only check for missing settings once
       await checkForMissingSettings()
     }
+    setInterval(() => {
+      if (bot.shards.filter(shard => shard.latency == Infinity && shard.status === 'disconnected').length !== 0) {
+        failedHealthCheckCount++
+        if (failedHealthCheckCount >= 5) {
+          global.logger.warn(`[${cluster.worker.rangeForShard}] Shard health check failed 5 times in a row, hard resetting shards`)
+          global.webhook.error(`[${cluster.worker.rangeForShard}] Shard health check failed 5 times in a row, hard resetting shards`)
+          bot.shards.forEach(shard => {
+            shard.hardReset()
+            shard.connect()
+          })
+        }
+      }
+    }, 1000 * 60)
   }
 }
--- src/bot/events/shardDisconnect.js
+++ src/bot/events/shardDisconnect.js
@@ -6,6 +6,6 @@ module.exports = {
     handle: async (err, shardID) => {
       const cachedShard = global.bot.shards.get(shardID)
       global.logger.info(`[${cluster.worker.rangeForShard}] Shard ${shardID} disconnected with error`, err)
-      global.webhook.warn(`[${cluster.worker.rangeForShard}] Shard ${shardID} disconnected with error: ${err?.message ?? 'no error message'}\n\nResume -> ${cachedShard.resumeURL}\nStatus -> ${cachedShard.status}\nReconnecting? -> ${cachedShard.connecting ? 'yes' : 'no'}\nLast heartbeat sent: <t:${Math.ceil(cachedShard.lastHeartbeatSent / 1000)}>\nLast heartbeat received: <t:${Math.ceil(cachedShard.lastHeartbeatReceived / 1000)}>`)
+      // global.webhook.warn(`[${cluster.worker.rangeForShard}] Shard ${shardID} disconnected with error: ${err?.message ?? 'no error message'}\n\nResume -> ${cachedShard.resumeURL}\nStatus -> ${cachedShard.status}\nReconnecting? -> ${cachedShard.connecting ? 'yes' : 'no'}\nLast heartbeat sent: <t:${Math.ceil(cachedShard.lastHeartbeatSent / 1000)}>\nLast heartbeat received: <t:${Math.ceil(cachedShard.lastHeartbeatReceived / 1000)}>`)
     }
   }
